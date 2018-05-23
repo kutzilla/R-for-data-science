d32fcf4fee2e200f50466eee6fe02552f4379083
Matthias Kutz <kutz.matthias@gmail.com>
2018-05-23 14:12:21 +0200

    Code aus Kapitel Workflow Basics hinzugefügt

---
+++ 1-explore/workflow_basics.R
@@ -0,0 +1,23 @@
+# Einfache Kalkulationen
+1 / 200 * 30
+
+(59 + 73 + 2) / 3
+
+sin(pi / 2)
+
+# Erstellen von Variablen
+
+x <- 3 * 4
+
+this_is_a_really_long_name <- 2.5
+
+r_rocks <- 2 ^ 3
+
+seq(1, 10)
+
+x <- "hello world"
+
+y <- seq(1, 10, length.out = 5)
+
+# Setzen der Variable mit Ausgabe am Bildschirm
+(y <- seq(1, 10, length.out = 5))
